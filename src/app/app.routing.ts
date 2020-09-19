cc21903e5bc3d57252e30543874efc74c68660ac
Berlad Gonzalez <berlad46@gmail.com>
2020-09-19 16:21:52 -0500

    commit desde rama master

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -14,7 +14,7 @@ import { SobreMiComponent } from './componentes/sobre-mi/sobre-mi.component';
 
 // Definiendo rutas
 const appRoutes: Routes = [
-	{path: '', component: InicioComponent},
+	{path: '', component: ErrorComponent},
 	{path: 'inicio', component: InicioComponent},
 	{path: 'sobre-mi', component: SobreMiComponent},
 	{path: 'estudios', component: EstudiosComponent},
